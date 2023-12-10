10bd0578651b5401d8c9fe3e75ef53024e6023f6
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2023-12-10 19:26:34 +0530

    Update colors.dart

--- demo/lib/src/constants/colors.dart
+++ demo/lib/src/constants/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const mPrimaryColor = Color.fromARGB(255, 247, 139, 24);
 const mSecondaryColor = Color.fromARGB(255, 0, 160, 120);
 const mAccentColor = Color.fromARGB(182, 120, 49, 1);
-
+//---on Boarding Colors
 const mWhiteColor = Colors.white;
 const mDarkColor = Colors.black87;
 const mCardBgColor = Color.fromARGB(255, 241, 247, 247);
