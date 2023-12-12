eb7c67884e938c22fe14813dc7987a13b7951b2e
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2023-12-12 23:16:08 +0530

    Update hcategory.dart

--- demo/lib/src/features/home/screen/category/model/hcategory.dart
+++ demo/lib/src/features/home/screen/category/model/hcategory.dart
@@ -17,7 +17,7 @@ class Category extends Equatable {
         imageUrl:
             'https://firebasestorage.googleapis.com/v0/b/phonenoproject.appspot.com/o/category%2FmomosT.png?alt=media&token=b34ffcd3-f47c-4c88-93e5-ef096c7fbe84'
         // 'https://img-global.cpcdn.com/recipes/aa12bb758db3d83c/1200x630cq70/photo.jpg'
-        // 'https://i0.wp.com/passion2cook.com/wp-content/uploads/2023/03/paneer-momos-1.jpg?resize=768%2C1024&ssl=1',
+        'https://i0.wp.com/passion2cook.com/wp-content/uploads/2023/03/paneer-momos-1.jpg?resize=768%2C1024&ssl=1',
         ),
     Category(
         name: "Noodles",
